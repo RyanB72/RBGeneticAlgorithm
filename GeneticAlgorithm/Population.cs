7ad3cda6929e1880e39314e6d72a8558602fea3a
Ryan <ryan.browning@student.adelaide.edu.au>
2021-10-18 14:40:09 +1030

    Files Added

---
+++ GeneticAlgorithm/Population.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneticAlgorithm
+{
+    /*
+    -This class represents a single piece of the population
+    -Its genes are held in 'set'. A size 10 integer array
+    -The genes repersent 2 numbers. Each with a single digit and 4 decimal places
+
+    for example the set [0,1,2,3,4,5,6,7,8,9] represents the following 2 numbers
+    x = 0.1234
+    y = 5.6789
+    */
+    public class Population
+    {
+        public int[] set = new int[10];
+        public double x;
+        public double y;
+        public double fitness;
+
+        //intialize the population to have a set
+        //Then calculate the numbers it represents
+        //Then calculate the fitness
+        public Population(int[] values)
+        {
+            set = values;
+            setDoubles(values);
+            calculateFitness();
+        }
+
+        //from the gene set, get the values x and y
+        private void setDoubles(int[] values)
+        {
+            //set the digit
+            x = values[0] * 10000;
+            y = values[5] * 10000;
+
+            for(int i=1; i<5; i++)
+            {
+                x += values[i] * Math.Pow(10, 4-i);
+            }
+            x = x / 10000;
+
+            for (int i = 6; i < 10; i++)
+            {
+                if(y < 0)
+                {
+                    y -= values[i] * Math.Pow(10, 9 - i);
+                } else
+                {
+                    y += values[i] * Math.Pow(10, 9 - i);
+                }
+                
+            }
+            y = y / 10000;
+        }
+
+        //Loop over all pieces of the set, by the probability of 'chance' randomize a piece
+        public void switchBit(Random rand, double chance)
+        {
+            bool changeHappened = false;
+            for(int i=0; i<set.Length; i++)
+            {
+                if(rand.NextDouble() < chance)
+                {
+                    set[i] = rand.Next(0, 10);
+                    changeHappened = true;
+                }
+            }
+            
+            //if the set has changed, recalculate values and fitness
+            if(changeHappened)
+            {
+                setDoubles(set);
+                calculateFitness();
+            }
+        }
+
+        /*
+        -Calculate the fitness of the set
+        -If the values are outside the allowed params, then set a useless fitness
+        -Because this is a minimization problem, a useless fitness is large (999)
+        */
+        private void calculateFitness()
+        {
+            // 0 <= x <= 1
+            if(x > 1 || x < 0)
+            {
+                fitness = 999;
+                return;
+            }
+
+            // -2 <= y <= 2
+            if (y > 2 || y < -2)
+            {
+                fitness = 999;
+                return;
+            }
+
+            //f(x) = x
+            //g(x, y) = 1 + Math.Pow(y,2) - x - 0.1 * Math.Sin(3 * Math.PI * x)
+            //fitness = f(x) + g(x, y)
+            fitness = x + 1 + Math.Pow(y, 2) - x - 0.1 * Math.Sin(3 * Math.PI * x);
+        }
+
+        //Display inline the set, values and fitness
+        //E.G. [0,7,7,9,3,0,3,5,3,4]    X = 0.7793      Y = 0.3534      Fitness = 1.037580626242288
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write(set[i] + ",");
+            }
+            Console.Write(set[9] + "]");
+
+            Console.WriteLine("\t X = "+x+ "\t Y = " + y + "\t Fitness = "+fitness);
+        }
+    }
+}
---
+++ GeneticAlgorithm/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneticAlgorithm
+{
+    class Program
+    {
+        public Population[] pool;
+        public int populationSize = 250;
+        public double mergeChance = 0.80;
+        public double bitSwitchChance = 0.02;
+        public int numberOfGen = 100;
+        public double currentBestFitness = 999;
+        public int bestStreak = 0;
+
+        static void Main(string[] _)
+        {
+            Random rand = new Random();
+            var mc = new Program();
+
+            //collect inputs
+            mc.Setup(mc);
+
+            //create initial pool
+            mc.pool = new Population[mc.populationSize];
+
+            //Fill the pool with random populations
+            for (int i=0; i<mc.pool.Length; i++)
+            {
+                mc.pool[i] = new Population(mc.fetchRandomArray(rand));
+            }
+
+            //from the pool, create the next generation 'numberOfGen' times
+            //Stops early if the best fitness has not changed in 25 generations
+            for(int i=0; i<mc.numberOfGen; i++)
+            {
+                mc.CreateNextPool(mc);
+
+                mc.calculateBest(mc);
+
+                if(mc.bestStreak >=25)
+                {
+                    Console.WriteLine("Best fitness has not change in 25 generations. Orginally found in Generation "+(i-25));
+                    break;
+                }
+            }
+        }
+
+        //Calculates the best fitness from the current pool and prints it
+        public void calculateBest(Program mc)
+        {
+            int posOfBest = 0;
+            for (int i = 0; i < mc.pool.Length; i++)
+            {
+                //if a smaller fitness is found, save its position in pool
+                if (mc.pool[i].fitness < mc.pool[posOfBest].fitness)
+                {
+                    posOfBest = i;
+                }
+            }
+
+            if (mc.currentBestFitness == mc.pool[posOfBest].fitness)
+            {
+                mc.bestStreak++;
+            } else
+            {
+                mc.bestStreak = 0;
+            }
+
+            mc.currentBestFitness = mc.pool[posOfBest].fitness;
+            mc.pool[posOfBest].Print();
+        }
+
+        //Collects inputs from console. The inputs are assumed correct and not checked
+        public void Setup(Program mc)
+        {
+            Console.Write("Enter Population Size (Must be Even)(recommended 250): ");
+            mc.populationSize = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Gene Merge Chance (between 0 and 1)(recommended 0.8): ");
+            mc.mergeChance = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Bit Switch Change Chance (between 0 and 1)(recommended 0.02): ");
+            mc.bitSwitchChance = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Maximum Number of Generations(recommended 100): ");
+            mc.numberOfGen = Convert.ToInt32(Console.ReadLine());
+        }
+
+        /*
+        -From the current pool it creates an equal sized pool where the children are stored
+        -At the end of the function the current pool is overwritten with the children pool
+        */
+        public void CreateNextPool(Program mc)
+        {
+            Random rand = new Random();
+
+            Population[] childPool = new Population[mc.pool.Length];
+
+            for (int i = 0; i < childPool.Length-1; i+=2)
+            {
+                //Tournament style parent selection (2 parents)
+                Population parentOne = mc.Fight(mc.pool[rand.Next(0, mc.pool.Length)], mc.pool[rand.Next(0, mc.pool.Length)]);
+                Population parentTwo = mc.Fight(mc.pool[rand.Next(0, mc.pool.Length)], mc.pool[rand.Next(0, mc.pool.Length)]);
+
+                //merge the parents if within probability
+                if(rand.NextDouble() < mc.mergeChance)
+                {
+                    childPool[i] = mc.Merge(parentOne, parentTwo);
+                    childPool[i + 1] = mc.Merge(parentTwo, parentOne);
+                } else //the children are the parents
+                {
+                    childPool[i] = parentOne;
+                    childPool[i + 1] = parentTwo;
+                }
+
+                childPool[i].switchBit(rand, mc.bitSwitchChance);
+                childPool[i+1].switchBit(rand, mc.bitSwitchChance);
+            }
+            //the current generation is overwritten with the new
+            mc.pool = childPool;
+        }
+
+        //Two populations are parsed (parents), return a single child where-
+        //the first half of its set is from parent a and second half from parent b
+        public Population Merge(Population a, Population b)
+        {
+            int[] arr = new int[10];
+            for(int i=0;i<5; i++)
+            {
+                arr[i] = a.set[i];
+            }
+            for (int i = 5; i < 10; i++)
+            {
+                arr[i] = b.set[i];
+            }
+            return new Population(arr);
+        }
+
+        //Two populations are parsed, the one with the smaller fitness is returned
+        public Population Fight(Population a, Population b)
+        {
+            if(a.fitness < b.fitness)
+            {
+                return a;
+            } else
+            {
+                return b;
+            }
+        }
+
+        //returns an int array of size 10 where most of the values are randomly between [0, 10)
+        //value a[0] is either 0 or 1. because any other value will exceed bounds
+        //value a[5] is between [-2,2] because any other value will exceed bounds
+        public int[] fetchRandomArray(Random rand)
+        {
+            int[] a = { rand.Next(0, 2), rand.Next(0, 10), rand.Next(0, 10), rand.Next(0, 10), rand.Next(0, 10), rand.Next(-2, 3), rand.Next(0, 10), rand.Next(0, 10), rand.Next(0, 10), rand.Next(0, 10)}; ;
+            return a;
+        }
+    }
+}
